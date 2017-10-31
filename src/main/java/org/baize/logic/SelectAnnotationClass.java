eb726132bf49d03c1012974a4437bca86101a733
yipp <1030681978@qq.com>
2017-10-31 18:09:52 +0800

    s

--- src/main/java/org/baize/logic/SelectAnnotationClass.java
+++ src/main/java/org/baize/logic/SelectAnnotationClass.java
@@ -7,7 +7,6 @@ import org.baize.annotation.Service;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.List;
